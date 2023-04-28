f2804b3ba937f524177738c4e94f1592d2fe5a44
TheGueroRex <luifermen07@gmail.com>
2023-04-28 15:55:03 -0600

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,145 @@
+<template>
+    <div>
+        <header>
+           <div class="brand">
+            <h1>Bank</h1>
+            <svg width="36" height="33" viewBox="0 0 36 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M18 0L22.0413 12.4377H35.119L24.5389 20.1246L28.5801 32.5623L18 24.8754L7.41987 32.5623L11.4611 20.1246L0.880983 12.4377H13.9587L18 0Z" fill="#9FC733"/>
+</svg>
+
+
+           </div>
+
+           <nav>
+            <ul>
+              <li>
+                <a href="">Home</a>
+              </li>
+              <li>
+                <a href="">Productos</a>
+                <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M0.276203 0.651245C0.634498 0.306731 1.20424 0.317902 1.54875 0.676197L6 5.4015L10.4513 0.676197C10.7958 0.317903 11.3655 0.306731 11.7238 0.651245C12.0821 0.995759 12.0933 1.5655 11.7487 1.92379L6.64875 7.32379C6.47907 7.50026 6.24482 7.6 6 7.6C5.75519 7.6 5.52093 7.50026 5.35125 7.32379L0.251251 1.92379C-0.0932627 1.5655 -0.0820913 0.995759 0.276203 0.651245Z" fill="#005B94" fill-opacity="0.75"/>
+</svg>
+
+              </li>
+              <li>
+                <a href="">Nosotros</a>
+                <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M0.276203 0.651245C0.634498 0.306731 1.20424 0.317902 1.54875 0.676197L6 5.4015L10.4513 0.676197C10.7958 0.317903 11.3655 0.306731 11.7238 0.651245C12.0821 0.995759 12.0933 1.5655 11.7487 1.92379L6.64875 7.32379C6.47907 7.50026 6.24482 7.6 6 7.6C5.75519 7.6 5.52093 7.50026 5.35125 7.32379L0.251251 1.92379C-0.0932627 1.5655 -0.0820913 0.995759 0.276203 0.651245Z" fill="#005B94" fill-opacity="0.75"/>
+</svg>
+
+              </li>
+              <li>
+                <a href="">Precios</a>
+                <svg width="12" height="8" viewBox="0 0 12 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M0.276203 0.651245C0.634498 0.306731 1.20424 0.317902 1.54875 0.676197L6 5.4015L10.4513 0.676197C10.7958 0.317903 11.3655 0.306731 11.7238 0.651245C12.0821 0.995759 12.0933 1.5655 11.7487 1.92379L6.64875 7.32379C6.47907 7.50026 6.24482 7.6 6 7.6C5.75519 7.6 5.52093 7.50026 5.35125 7.32379L0.251251 1.92379C-0.0932627 1.5655 -0.0820913 0.995759 0.276203 0.651245Z" fill="#005B94" fill-opacity="0.75"/>
+</svg>
+
+              </li>
+            </ul>
+           </nav>
+            
+           
+
+           <div class="button">
+            <button>
+              Acceder
+              <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M14.4831 15.1644C13.2278 13.5077 11.2389 12.4375 9 12.4375C6.76114 12.4375 4.77223 13.5077 3.51692 15.1644M14.4831 15.1644C16.1807 13.6533 17.25 11.4515 17.25 9C17.25 4.44365 13.5563 0.75 9 0.75C4.44365 0.75 0.75 4.44365 0.75 9C0.75 11.4515 1.81929 13.6533 3.51692 15.1644M14.4831 15.1644C13.0255 16.4618 11.1048 17.25 9 17.25C6.89518 17.25 4.97446 16.4618 3.51692 15.1644M11.75 6.9375C11.75 8.45628 10.5188 9.6875 9 9.6875C7.48122 9.6875 6.25 8.45628 6.25 6.9375C6.25 5.41872 7.48122 4.1875 9 4.1875C10.5188 4.1875 11.75 5.41872 11.75 6.9375Z" stroke="white" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+            </button>
+           </div>
+        </header>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+//   props: {
+//     msg: String
+//   }
+}
+</script>
+
+<style scoped>
+
+
+h1{
+    font-family: Arial, Helvetica, sans-serif;
+    text-transform: uppercase;
+    font-size: 2em;
+}
+
+header{
+    width: 100%;
+    height: 100px;
+    display: flex;
+    justify-content:space-around;
+    align-items: center
+}
+
+ nav{
+  width: 80%;
+  height: 80%;
+  
+ }
+
+ ul{
+    display: flex;
+    width: 70%;
+    height: 80%;
+    justify-content: space-around;
+    align-items: center;    
+ }
+
+ li{
+    list-style: none;
+    width: 15%;
+    height: 40%;
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10%;  
+ }
+ a{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1em;
+    color: #005B94;
+    text-decoration: none;
+    
+ }
+
+ li:hover{
+    font-weight: 600;
+    
+ }
+ li:nth-child(1):hover{
+    border-bottom: 3px solid #005B94;
+ }
+
+
+ .button,
+ .brand{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 10%;
+    height: 80%;
+ }
+ button{
+    background: #005B94;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 10%;
+    width: 9em;
+    height: 3em;
+    border: none;
+    border-radius: 5px;
+    color: white;
+ }
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,299 @@
+<template>
+    <section class="home">
+        <div class="home-main">
+            <div class="message-home">
+                <p>Nuestro negocio es hacer <span>crecer el tuyo.</span></p>
+                <div class="btn-home">
+                     <button> Contacta a un ejecutivo 
+                        <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M5.00003 4.58107C6.2624 4.58107 7.28575 3.55772 7.28575 2.29536C7.28575 1.03299 6.2624 0.00964355 5.00003 0.00964355C3.73767 0.00964355 2.71432 1.03299 2.71432 2.29536C2.71432 3.55772 3.73767 4.58107 5.00003 4.58107Z" fill="black"/>
+                        <path d="M10.7143 5.34298C11.7663 5.34298 12.6191 4.49019 12.6191 3.43821C12.6191 2.38624 11.7663 1.53345 10.7143 1.53345C9.66235 1.53345 8.80956 2.38624 8.80956 3.43821C8.80956 4.49019 9.66235 5.34298 10.7143 5.34298Z" fill="black"/>
+                        <path d="M0.897389 11.0024C0.594681 10.8086 0.419346 10.4638 0.46369 10.1071C0.744154 7.85118 2.66818 6.10488 4.99996 6.10488C7.33174 6.10488 9.25576 7.8507 9.53622 10.1065C9.58057 10.4632 9.40523 10.808 9.10254 11.0018C7.9183 11.7601 6.51046 12.2001 4.99996 12.2001C3.48946 12.2001 2.08163 11.7606 0.897389 11.0024Z" fill="black"/>
+                        <path d="M10.7144 10.6763C10.6875 10.6763 10.6606 10.6761 10.6338 10.6757C10.6862 10.4496 10.7008 10.2104 10.6703 9.96552C10.5367 8.89058 10.1049 7.90834 9.46023 7.10423C9.84923 6.95097 10.273 6.86679 10.7165 6.86679C12.1944 6.86679 13.4539 7.80189 13.9356 9.11271C14.0391 9.39445 13.9178 9.70417 13.6607 9.85916C12.8004 10.3779 11.7922 10.6763 10.7144 10.6763Z" fill="black"/>
+                        </svg>
+                    </button>
+                </div>
+                
+            </div>
+            <img src="../assets/pexels-fauxels-3184405.jpg" alt="">
+        </div>
+        
+        <div class="home-description">
+            <h2>Soluciones financieras efectivas</h2>
+            <p>Credito PyME, inversion, paneles solares</p>
+        </div>
+        
+    </section>
+    <section class="credits">
+        <div class="credits-description">
+            <h2>Liquidez inmediata para tu <span>negocio.</span></h2>
+            <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat vero magnam nesciunt eligendi, a in quasi ipsam id ab atque animi optio. Enim esse modi reiciendis assumenda qui eaque officia. </p>
+
+        </div>
+        <div class="credits-cards">
+            <div class="card">
+                <div class="icon">
+                    <svg width="21" height="13" viewBox="0 0 21 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M13.8804 0.999434C14.0208 0.633566 14.4313 0.450824 14.7971 0.591267L20.4183 2.74903C20.594 2.81648 20.7357 2.95095 20.8122 3.12288C20.8888 3.2948 20.8939 3.4901 20.8265 3.66579L18.6687 9.28697C18.5283 9.65283 18.1178 9.83558 17.7519 9.69513C17.3861 9.55469 17.2033 9.14424 17.3438 8.77838L18.8876 4.75653L17.8596 5.21421C15.5386 6.24761 13.7708 8.00815 12.703 10.109C12.6 10.3116 12.4062 10.4526 12.1818 10.4883C11.9573 10.524 11.7294 10.45 11.5687 10.2893L7.99601 6.71661L2.11144 12.6012C1.83432 12.8783 1.38503 12.8783 1.10792 12.6012C0.830807 12.3241 0.830807 11.8748 1.10792 11.5977L7.49425 5.21134C7.62733 5.07827 7.80782 5.00351 7.99601 5.00351C8.18421 5.00351 8.36469 5.07827 8.49777 5.21134L11.9095 8.62306C13.14 6.61623 14.9699 4.94729 17.2824 3.91772L18.3104 3.46003L14.2885 1.91619C13.9227 1.77575 13.7399 1.3653 13.8804 0.999434Z" fill="black"/>
+</svg>
+                </div>
+                
+
+                <h2>
+                    CREDITO 1
+                </h2>
+                <P>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam modi nobis odio perferendis dicta nam harum amet, iusto pariatur numquam reprehenderit ipsum consectetur in explicabo impedit, at esse incidunt voluptatum!
+
+                </P>
+                <a href="">Más info <span><svg width="13" height="7" viewBox="0 0 13 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M9.1372 0.644172C9.32194 0.45943 9.62147 0.45943 9.80621 0.644172L12.1715 3.00948C12.3563 3.19422 12.3563 3.49375 12.1715 3.67849L9.80621 6.0438C9.62147 6.22854 9.32194 6.22854 9.1372 6.0438C8.95246 5.85906 8.95246 5.55953 9.1372 5.37479L10.6949 3.81705H0.483529C0.222264 3.81705 0.0104675 3.60525 0.0104675 3.34399C0.0104675 3.08272 0.222264 2.87092 0.483529 2.87092H10.6949L9.1372 1.31318C8.95246 1.12844 8.95246 0.828914 9.1372 0.644172Z" fill="#005B94"/>
+</svg>
+</span></a>
+
+            </div>
+            <div class="card">
+                <div class="icon">
+                    <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M13.6203 12.6646C13.719 13.084 13.7713 13.5212 13.7713 13.9707C13.7713 17.1058 11.2297 19.6474 8.09452 19.6474V15.1063M13.6203 12.6646C17.1531 10.0832 19.448 5.9085 19.448 1.198C14.7377 1.1982 10.5634 3.49316 7.98214 7.02588M13.6203 12.6646C12.0038 13.8459 10.1281 14.6935 8.09452 15.1063M7.98214 7.02588C7.56259 6.92704 7.12507 6.87474 6.67534 6.87474C3.54016 6.87474 0.998596 9.4163 0.998596 12.5515H5.54067M7.98214 7.02588C6.80103 8.64235 5.95345 10.518 5.54067 12.5515M8.09452 15.1063C7.99668 15.1262 7.89848 15.145 7.79992 15.1629C6.94187 14.4825 6.16434 13.705 5.48397 12.8469C5.50185 12.7481 5.52076 12.6496 5.54067 12.5515M3.42248 14.8134C2.38196 15.5896 1.70819 16.8303 1.70819 18.2282C1.70819 18.4523 1.7255 18.6724 1.75887 18.8871C1.97362 18.9205 2.19368 18.9378 2.41778 18.9378C3.81575 18.9378 5.05635 18.264 5.83257 17.2235M14.4809 7.58433C14.4809 8.36813 13.8455 9.00352 13.0617 9.00352C12.2779 9.00352 11.6425 8.36813 11.6425 7.58433C11.6425 6.80054 12.2779 6.16515 13.0617 6.16515C13.8455 6.16515 14.4809 6.80054 14.4809 7.58433Z" stroke="black" stroke-width="1.41919" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+                </div>
+                
+
+                <h2>
+                    CREDITO 2
+                </h2>
+                <P>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam modi nobis odio perferendis dicta nam harum amet, iusto pariatur numquam reprehenderit ipsum consectetur in explicabo impedit, at esse incidunt voluptatum!
+
+                </P>
+                <a href="">Más info <span><svg width="13" height="7" viewBox="0 0 13 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M9.1372 0.644172C9.32194 0.45943 9.62147 0.45943 9.80621 0.644172L12.1715 3.00948C12.3563 3.19422 12.3563 3.49375 12.1715 3.67849L9.80621 6.0438C9.62147 6.22854 9.32194 6.22854 9.1372 6.0438C8.95246 5.85906 8.95246 5.55953 9.1372 5.37479L10.6949 3.81705H0.483529C0.222264 3.81705 0.0104675 3.60525 0.0104675 3.34399C0.0104675 3.08272 0.222264 2.87092 0.483529 2.87092H10.6949L9.1372 1.31318C8.95246 1.12844 8.95246 0.828914 9.1372 0.644172Z" fill="#005B94"/>
+</svg>
+</span></a>
+
+            </div>
+            <div class="card">
+                <div class="icon">
+                    <svg width="21" height="19" viewBox="0 0 21 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M7.06438 17.9377V13.3253C7.06438 12.7375 7.54092 12.261 8.12876 12.261H10.2575C10.8454 12.261 11.3219 12.7375 11.3219 13.3253V17.9377M11.3219 17.9377H15.5795V1.42354M11.3219 17.9377H18.4179V8.23993M1.38763 17.9377H2.80682M19.837 17.9377H2.80682M1.38763 6.58421L5.64519 5.03601M16.9987 0.907471L15.5795 1.42354M15.5795 7.2938L18.4179 8.23993M19.837 8.71299L18.4179 8.23993M5.64519 5.03601V0.907471H2.80682V17.9377M5.64519 5.03601L15.5795 1.42354" stroke="black" stroke-width="1.41919" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+                </div>
+                
+
+                <h2>
+                    CREDITO 3
+                </h2>
+                <P>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam modi nobis odio perferendis dicta nam harum amet, iusto pariatur numquam reprehenderit ipsum consectetur in explicabo impedit, at esse incidunt voluptatum!
+
+                </P>
+                <a href="">Más info <span><svg width="13" height="7" viewBox="0 0 13 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M9.1372 0.644172C9.32194 0.45943 9.62147 0.45943 9.80621 0.644172L12.1715 3.00948C12.3563 3.19422 12.3563 3.49375 12.1715 3.67849L9.80621 6.0438C9.62147 6.22854 9.32194 6.22854 9.1372 6.0438C8.95246 5.85906 8.95246 5.55953 9.1372 5.37479L10.6949 3.81705H0.483529C0.222264 3.81705 0.0104675 3.60525 0.0104675 3.34399C0.0104675 3.08272 0.222264 2.87092 0.483529 2.87092H10.6949L9.1372 1.31318C8.95246 1.12844 8.95246 0.828914 9.1372 0.644172Z" fill="#005B94"/>
+</svg>
+</span></a>
+
+            </div>
+            <div class="card">
+                <div class="icon">
+                    <svg width="20" height="19" viewBox="0 0 20 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.4205 17.9377V10.8418C11.4205 10.4499 11.7382 10.1322 12.1301 10.1322H14.9685C15.3604 10.1322 15.6781 10.4499 15.6781 10.8418V17.9377M11.4205 17.9377H0.881516M11.4205 17.9377H15.6781M15.6781 17.9377H19.1211M17.8068 17.9377V6.91418M2.1958 17.9377V6.91442M2.1958 6.91442C3.28128 7.54183 4.69335 7.3912 5.62201 6.46254C5.66423 6.42032 5.70484 6.37711 5.74384 6.33297C6.26393 6.92218 7.02484 7.29379 7.87254 7.29379C8.72028 7.29379 9.48123 6.92214 10.0013 6.33288C10.5214 6.92214 11.2823 7.29379 12.1301 7.29379C12.9777 7.29379 13.7386 6.92222 14.2587 6.33308C14.2977 6.37715 14.3382 6.4203 14.3804 6.46246C15.3091 7.39119 16.7213 7.54177 17.8068 6.91418M2.1958 6.91442C1.98565 6.79295 1.78774 6.64233 1.60795 6.46254C0.499498 5.35408 0.499498 3.55693 1.60795 2.44848L2.73316 1.32326C2.99931 1.05711 3.36029 0.907593 3.73668 0.907593H16.2658C16.6422 0.907593 17.0031 1.05711 17.2693 1.32326L18.3944 2.44839C19.5029 3.55685 19.5029 5.354 18.3944 6.46246C18.2147 6.64217 18.0169 6.79275 17.8068 6.91418M5.03417 15.0993H8.58213C8.97403 15.0993 9.29172 14.7816 9.29172 14.3897V10.8418C9.29172 10.4499 8.97403 10.1322 8.58213 10.1322H5.03417C4.64227 10.1322 4.32457 10.4499 4.32457 10.8418V14.3897C4.32457 14.7816 4.64227 15.0993 5.03417 15.0993Z" stroke="black" stroke-width="1.41919" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+                </div>
+                
+
+                <h2>
+                    CREDITO 4
+                </h2>
+                <P>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam modi nobis odio perferendis dicta nam harum amet, iusto pariatur numquam reprehenderit ipsum consectetur in explicabo impedit, at esse incidunt voluptatum!
+
+                </P>
+                <a href="">Más info <span><svg width="13" height="7" viewBox="0 0 13 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M9.1372 0.644172C9.32194 0.45943 9.62147 0.45943 9.80621 0.644172L12.1715 3.00948C12.3563 3.19422 12.3563 3.49375 12.1715 3.67849L9.80621 6.0438C9.62147 6.22854 9.32194 6.22854 9.1372 6.0438C8.95246 5.85906 8.95246 5.55953 9.1372 5.37479L10.6949 3.81705H0.483529C0.222264 3.81705 0.0104675 3.60525 0.0104675 3.34399C0.0104675 3.08272 0.222264 2.87092 0.483529 2.87092H10.6949L9.1372 1.31318C8.95246 1.12844 8.95246 0.828914 9.1372 0.644172Z" fill="#005B94"/>
+</svg>
+</span></a>
+
+            </div>
+        </div>
+
+    </section>
+    
+</template>
+
+<script>
+export default {
+  name: 'Home',
+//   props: {
+//     msg: String
+//   }
+}
+</script>
+
+<style>
+ section{
+    width: 100%;
+    height: 100vh;
+ }
+
+ .home{
+    overflow: hidden;
+    background-image: url(../assets/bg-home.svg);
+    background-repeat: no-repeat;
+    background-size: 120%;
+    background-position-x: 50%;
+    background-position-y: 130%;
+ }
+
+ .home-main{
+    position: relative;
+    display: flex;
+    width: 100%;
+    height: 70%;   
+ }
+
+ .message-home{
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-end;
+    width: 55%;
+    height: 100%;
+    font-family: Arial, Helvetica, sans-serif;
+ }
+ .message-home p{
+    
+    font-size: 4em;
+    font-weight: 600;
+    width: 70%;
+ }
+
+ .message-home p span{
+    color: #002F51;
+ }
+
+ .btn-home{
+    width: 70%;
+ }
+ .btn-home button{
+    background: #9FC733;
+    border: none;
+    width: 15em;
+    height: 3em;
+    color: black;
+    font-weight: 600;
+    border-radius: 20px;
+ }
+
+ .message-home button svg{
+    transform: translateY(10%);
+    margin-left: 5%;
+ }
+
+
+ .home-main img{
+    position: absolute;
+    width: 50%;
+    transform: translateX(115%);
+    border-radius: 10px;
+ }
+
+ .home-description{
+    width: 50%;
+    margin: 0 auto;
+    text-align: center;
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+ }
+
+ .home-description h2{
+    font-size: 3em;
+ }
+
+ .credits{
+    transform: translateY(-3%);
+    background: #0A142F; 
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+ }
+
+ .credits-description{
+    width: 80%;
+    color: white;
+    font-family: Arial, Helvetica, sans-serif;
+ }
+
+ .credits-description h2{
+    font-size: 3em;
+    width: 30%;   
+ }
+
+ .credits-description p{
+    width: 70%;
+ }
+
+ .credits-description h2 span{
+    color: #9FC733;
+ }
+
+ .credits-cards{
+    display: flex;
+    justify-content: space-between;
+    margin-top: 2%;
+    width: 80%;
+ }
+
+ .card{
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background: white;
+    width: 22%;
+    height: 420px;
+    border-radius: 20px;
+    font-family: Arial, Helvetica, sans-serif;
+    gap: 5%;
+ }
+
+ .card h2{
+    width: 80%;
+    height: 10%;
+    margin: 0 auto;
+    border-bottom: 2px solid black;
+    color: #002F51;
+    font-size: 1em;
+ }
+
+ .card p{
+    font-size: 1em;
+    width: 80%;
+    margin: 0 auto;
+    
+ }
+
+ .card a{
+    transform: translate(60% , 250%);
+    text-decoration: none;
+    color: #005B94;
+ }
+
+ .icon{
+    width: 40px;
+    height: 40px;
+    text-align: center;
+    line-height: 40px;
+    box-shadow: 0px 0px 1px black;
+    border-radius: 5px;
+    transform: translateX(90%);
+ }
+
+</style>
+
+
